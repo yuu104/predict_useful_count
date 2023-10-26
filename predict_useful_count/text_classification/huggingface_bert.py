89d933983a93405cd2082aaecc223767dbaacb65
yuu <yusei8171@gmail.com>
2023-10-26 17:11:20 +0900

    一旦学習〜評価の出力まで実装完了

--- predict_useful_count/text_classification/huggingface_bert.py
+++ predict_useful_count/text_classification/huggingface_bert.py
@@ -1,14 +1,25 @@
 import os
+from typing import List, Tuple
 import pandas as pd
+from pandas import DataFrame
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from transformers import TrainingArguments
-from transformers import Trainer
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    ConfusionMatrixDisplay,
+    confusion_matrix,
+    classification_report,
+)
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+    pipeline,
+)
 from datasets import Dataset, DatasetDict
 
 
@@ -23,7 +34,86 @@ def compute_metrics(pred):
     return {"accuracy": acc, "f1": f1}
 
 
-def main():
+def get_train_test_split(review_df: DataFrame) -> Tuple[DataFrame, DataFrame]:
+    """
+    データを学習用・テスト用に分割する
+    """
+    train_df, test_df = train_test_split(review_df, test_size=0.2, random_state=42)
+    print("学習用データ")
+    print(train_df.groupby("label").size().reset_index(name="RecordCount"))
+    print("テスト用データ")
+    print(test_df.groupby("label").size().reset_index(name="RecordCount"))
+    return train_df, test_df
+
+
+def plot_confusion_matrix(y_pred: list, y_true: list, labels: List[str]):
+    """
+    混同行列をプロットする
+    """
+
+    cm = confusion_matrix(y_true, y_pred, normalize="true", labels=labels)
+    fig, ax = plt.subplots(figsize=(6, 6))
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap="Blues", values_format=".2f", ax=ax, colorbar=False)
+    plt.title("Normalized confusion matrix")
+    plt.show()
+
+
+def get_valuation_index(y_pred: list, y_true: list, labels: List[str]) -> DataFrame:
+    """
+    評価指標を取得する
+    """
+
+    report = classification_report(
+        y_true, y_pred, target_names=labels, output_dict=True
+    )
+    report_df = pd.DataFrame(report).T
+    return report_df
+
+
+def evaluation():
+    """
+    モデルの評価を行い、結果を出力する
+    """
+
+    category_name = "chocolate"
+    review_df = pd.read_csv(
+        f"{current_path}/../csv/text_classification/{category_name}/review.csv"
+    )
+    train_df, test_df = get_train_test_split(review_df=review_df)
+    train_df = train_df.drop(
+        train_df.columns[train_df.columns.str.contains("unnamed:", case=False)], axis=1
+    )
+    test_df = test_df.drop(
+        test_df.columns[test_df.columns.str.contains("unnamed:", case=False)], axis=1
+    )
+
+    y_true = []
+    y_pred = []
+    labels = ["0", "1~2", "3~4", "5~6", "7~9", "10~"]
+
+    classifier = pipeline("sentiment-analysis", model=f"{current_path}/models")
+    for item in zip(test_df["label"], test_df["text"]):
+        label = item[0]
+        text = item[1]
+
+        prediction = classifier(text)
+
+        y_true.append(str(label))
+        y_pred.append(str(prediction[0]["label"]))
+
+    valuation_index = get_valuation_index(y_pred=y_pred, y_true=y_true, labels=labels)
+    print(valuation_index)
+    plot_confusion_matrix(y_pred=y_pred, y_true=y_true, labels=labels)
+
+
+def training():
+    """
+    役立ち数の推論モデルを構築する
+    - 事前学習済みモデルを使用してレビュー文をベクトル化・ファインチューニング
+    - 学習したモデルを保存する
+    """
+
     category_name = "chocolate"
     pre_train_model_name = "cl-tohoku/bert-base-japanese-whole-word-masking"
 
@@ -31,7 +121,7 @@ def main():
     review_df = pd.read_csv(
         f"{current_path}/../csv/text_classification/{category_name}/encoded_review.csv"
     )
-    train_df, test_df = train_test_split(review_df, test_size=0.2, random_state=42)
+    train_df, test_df = get_train_test_split(review_df=review_df)
     train_df = train_df.drop(
         train_df.columns[train_df.columns.str.contains("unnamed:", case=False)], axis=1
     )
@@ -66,9 +156,9 @@ def main():
     # 学習の準備
     batch_size = 8
     logging_steps = len(dataset_encoded["train"])  # batch_size
-    model_name = f"{current_path}/../model_outputs/chocolate"
+    output_dir = f"{current_path}/model_outputs/{category_name}/{pre_train_model_name}"
     training_args = TrainingArguments(
-        output_dir=model_name,
+        output_dir=output_dir,
         num_train_epochs=2,
         learning_rate=2e-5,
         per_device_train_batch_size=batch_size,
@@ -93,11 +183,12 @@ def main():
     trainer.train()
 
     # モデルの保存
-    trainer.save_model(f"{current_path}/../models")
+    trainer.save_model(f"{current_path}/models/{category_name}/{pre_train_model_name}")
 
-    # preds_output = trainer.predict(dataset_encoded["test"])
-    # y_preds = np.argmax(preds_output.predictions, axis=1)
-    # y_valid = np.array(dataset_encoded["test"]["label"])
+
+def main():
+    # training()
+    evaluation()
 
 
 if __name__ == "__main__":
