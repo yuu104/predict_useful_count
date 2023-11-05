7fe29e0f218926267c21863eeeab3241a0086993
yuu <yusei8171@gmail.com>
2023-11-05 15:57:54 +0900

    レビューデータを学習用とテスト用に分割し、csvに保存する関数を utils/ に作成した

--- predict_useful_count/utils/dataset.py
+++ predict_useful_count/utils/dataset.py
@@ -1,7 +1,11 @@
 from typing import Tuple
 from pandas import DataFrame
+import os
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import train_test_split
+import pandas as pd
+
+current_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_train_test_split(df: DataFrame) -> Tuple[DataFrame, DataFrame]:
@@ -58,3 +62,28 @@ def under_sampling(df: DataFrame, strategy: dict) -> DataFrame:
     rus = RandomUnderSampler(random_state=0, sampling_strategy=strategy)
     resampled_train_df, _ = rus.fit_resample(X=df, y=y)
     return resampled_train_df
+
+
+def create_train_test_csv(category_name: str) -> None:
+    """
+    レビューデータを学習用とテスト用に分割し、csvに保存する
+
+    Parameters
+    ----------
+    category_name: str
+      対象カテゴリ
+    """
+
+    review_df = pd.read_csv(
+        f"{current_path}/../csv/text_classification/{category_name}/encoded_review.csv"
+    )
+    train_df, test_df = get_train_test_split(df=review_df)
+    train_df = under_sampling(
+        df=train_df, strategy={0: 2400, 1: 2400, 2: 2400, 3: 2400, 4: 2400, 5: 2400}
+    )
+    test_df = under_sampling(
+        df=test_df,
+        strategy={0: 250, 1: 250, 2: 250, 3: 250, 4: 250, 5: 250},
+    )
+    train_df.to_csv(f"{current_path}/../csv/text_classification/all/train.csv")
+    test_df.to_csv(f"{current_path}/../csv/text_classification/all/test.csv")
